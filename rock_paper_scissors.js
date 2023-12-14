1da5a958456b07214ac28873d9644352f3a4ae24
grow-tesk <git config --global user.email zweinman@hotmail.com>
2023-12-14 14:44:43 -0500

    Set algorithm for rock paper scissors.
    Algorithm to play single match of RPS has been completed.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,25 +1,29 @@
-const rPS = ["rock", "paper", "scissors"];
+const rPS = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    console.log(rPS[Math.floor(Math.random() * rPS.length)]);
+    return rPS[Math.floor(Math.random() * rPS.length)];
 };
 
 function playRound(playerSelection, computerSelection) {
     if (
-        playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock"
-       ) {
-        return "You Win!"
-    } else if (
-        playerSelection === "rock" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "rock"
+        playerSelection == 'rock' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'paper' ||
+        playerSelection == 'paper' && computerSelection == 'rock'
     ) {
-        return "You Lose. :("
+        return 'You win!'
+    } else if (
+        playerSelection == 'rock' && computerSelection == 'paper' ||
+        playerSelection == 'paper' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'rock'
+    ) {
+        return 'Sorry, you lose. :('
     } else {
-        return "You've Tied, Try Again."
+        return 'You\'ve tied.'
     }
   };
 
-  const playerSelection = "scissors";
+
+  const playerSelection = 'paper'
   const computerSelection = getComputerChoice();
+
   console.log(playRound(playerSelection, computerSelection));
