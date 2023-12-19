01ae2cb03c73a89d712fb9716426f0e2b4756cee
grow-tesk <git config --global user.email zweinman@hotmail.com>
2023-12-19 12:38:20 -0500

    Pass result of playRound function into game function
    Began process of coding algorithm for game(). Successfully passed result
    of playRound function into game function.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -10,20 +10,27 @@ function playRound(playerSelection, computerSelection) {
         playerSelection == 'scissors' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'rock'
     ) {
-        return 'You win!'
+        return "You win! You get a point.";
     } else if (
         playerSelection == 'rock' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'rock'
     ) {
-        return 'Sorry, you lose. :('
+        return "The computer won this round. It get's a point.";
     } else {
-        return 'You\'ve tied.'
+        return "You tied. Neither gets a point."
+  };
+};
+
+  function game() {
+    if (playRound(playerSelection, computerSelection) == 'You win! You get a point.') {
+        return 'hello.'
     }
   };
 
-
-  const playerSelection = 'paper'
+  const playerSelection = 'rock'
   const computerSelection = getComputerChoice();
 
   console.log(playRound(playerSelection, computerSelection));
+
+  
