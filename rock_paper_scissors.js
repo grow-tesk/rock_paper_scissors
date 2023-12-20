7808401967c20b97b3488c40d23c4942f1ef6566
grow-tesk <git config --global user.email zweinman@hotmail.com>
2023-12-20 14:20:06 -0500

    Oh ffs.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,49 +4,55 @@ function getComputerChoice() {
     return rPS[Math.floor(Math.random() * rPS.length)];
 };
 
-let userInput = prompt("Let's play Rock Paper Scissors.");
-  
-const playerSelection = userInput.toLowerCase();
+let userInput = prompt("Let's play Rock, Paper, Scissors.");
+
+  let playerScore = 0;
+  let computerScore = 0;
+
 const computerSelection = getComputerChoice();
 
-let playerScore = 0;
-let computerScore = 0;
+function game(playerScore, computerScore) {
 
-function playRound(playerSelection, computerSelection) {
+  let playerSelection = userInput;
+
+  //This is theoretically a full game.
+ for (playerScore >= 0 && computerScore >= 0; playerScore == 3 || computerScore == 3;) {
+
+  if (playRound(playerSelection, computerSelection) == 2) {
+    playerScore++;
+    playerSelection = prompt(`You won this round! Current Score: You:${playerScore}, Computer:${computerScore}.`)
+  } else if (playRound(playerSelection, computerSelection) == 1) {
+    computerScore++;
+    playerSelection = prompt(`You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}.`)
+  } else if (playRound(playerSelection, computerSelection) == 0) {
+    playerSelection = prompt(`You tied this round. Current Score: You:${playerScore}, Computer:${computerScore}`)
+  };
+
+};
+
+  //This is the function for 1 round of RPS
+  function playRound(playerSelection, computerSelection) {
     if (
         playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'rock'
     ) {
-        playerScore++;
-        return `You won this round! Current Score: You:${playerScore}, Computer:${computerScore}`;
+        return 2;
     } else if (
         playerSelection == 'rock' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'rock'
     ) {
-        computerScore++;
-        return `You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}`;
+        return 1;
     } else if (
         playerSelection == 'rock' && computerSelection == 'rock' ||
         playerSelection == 'scissors' && computerSelection == 'scissors' ||
         playerSelection == 'paper' && computerSelection == 'paper'
      ) {
-        return `You tied this round. Current Score: You:${playerScore}, Computer:${computerScore}`;
+        return 0;
     } else {
-        return "Please enter either Rock, Paper, or Scissors."
+        return "Please enter either Rock, Paper, or Scissors.";
     };
 };
 
-function game() {
-    if (playRound(playerSelection, computerSelection) == `You won this round! Current Score: You:${playerScore}, Computer:${computerScore}`) {
-        return "hello."
-    } else if (playRound(playerSelection, computerSelection) == `You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}`) {
-        return "goodbye."
-    };
- };
-
-  
-  console.log(playRound(playerSelection, computerSelection));
-
-  
+};
