8e7a8502253ece936f8965c9d7b43f10ef07b39e
grow-tesk <git config --global user.email zweinman@hotmail.com>
2023-12-20 10:51:07 -0500

    Apportion score
    Added score increment to playRound.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,44 +4,49 @@ function getComputerChoice() {
     return rPS[Math.floor(Math.random() * rPS.length)];
 };
 
+let userInput = prompt("Let's play Rock Paper Scissors.");
+  
+const playerSelection = userInput.toLowerCase();
+const computerSelection = getComputerChoice();
+
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (
         playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'rock'
     ) {
-        return "You win! You get a point.";
+        playerScore++;
+        return `You won this round! Current Score: You:${playerScore}, Computer:${computerScore}`;
     } else if (
         playerSelection == 'rock' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'rock'
     ) {
-        return "The computer won this round. It get's a point.";
+        computerScore++;
+        return `You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}`;
+    } else if (
+        playerSelection == 'rock' && computerSelection == 'rock' ||
+        playerSelection == 'scissors' && computerSelection == 'scissors' ||
+        playerSelection == 'paper' && computerSelection == 'paper'
+     ) {
+        return `You tied this round. Current Score: You:${playerScore}, Computer:${computerScore}`;
     } else {
-        return "You tied. Neither gets a point."
-  };
+        return "Please enter either Rock, Paper, or Scissors."
+    };
 };
 
-  function game(playerScore, computerScore) {
-
-    if (playRound(playerSelection, computerSelection) == 'You win! You get a point.') {
-      for (playerScore = 1; playerScore < 3; playerScore++) {
-        userInput = prompt(`You win this round. You have ${playerScore} points, and the computer has ${computerScore} points.`);
-      };
-    } else if (playRound(playerSelection, computerSelection) == 'The computer won this round. It get\'s a point.') {
-        return computerScore + 1;
-    } else {
-        return "You tied. Neither gets a point.";
+function game() {
+    if (playRound(playerSelection, computerSelection) == `You won this round! Current Score: You:${playerScore}, Computer:${computerScore}`) {
+        return "hello."
+    } else if (playRound(playerSelection, computerSelection) == `You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}`) {
+        return "goodbye."
     };
+ };
 
-    
-  };
-
-  let userInput = prompt("Let's play Rock Paper Scissors.");
   
-  const playerSelection = userInput.toLowerCase();
-  const computerSelection = getComputerChoice();
-
   console.log(playRound(playerSelection, computerSelection));
 
   
