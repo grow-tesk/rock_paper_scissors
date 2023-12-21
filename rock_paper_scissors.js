1ad05850c2b090629a755aaa1bcb932903a2faf4
grow-tesk <git config --global user.email zweinman@hotmail.com>
2023-12-21 14:57:52 -0500

    Correctly code RPS algorithm
    At the point where I can run automatically through a few rounds of RPS.
    Still need to figure out how to successfully put a limit on the loop.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,15 +4,50 @@ function getComputerChoice() {
     return rPS[Math.floor(Math.random() * rPS.length)];
 };
 
-const computerSelection = getComputerChoice();
-
 let playerScore = 0;
 let computerScore = 0;
 
 function game() {
+
+  userInput = prompt("Please enter either Rock, Paper, or Scissors below.");
+
+   for (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors';;) {
+
+    computerChoice = getComputerChoice();
+    
+    playRound(userInput, computerChoice);
+
+    if (playerScore > 2) {
+      alert("Congrats! You've won this game.");
+      break;
+    } else if(computerScore > 2) {
+      alert("Sorry. You've lost this game.")
+      break;
+    } else if (playRound(userInput, computerChoice) == 2) {
+      playerScore++;
+      userInput = prompt(`You won this round! Current Score: You:${playerScore}, Computer:${computerScore}`);
+    } else if (playRound(userInput, computerChoice) == 1) {
+      computerScore++;
+      userInput = prompt(`You lose this round. Current Score: You:${playerScore}, Computer:${computerScore}`);
+    } else if (playRound(userInput, computerChoice) == 0) {
+      userInput = prompt(`You tied this round. Current Score: You:${playerScore}, Computer:${computerScore}`);
+    } else {
+      userInput = prompt("Please enter either rock, paper, or scissors.");
+    };
+    
+  };
+};
+
+
+
+
+
+
+
+/*function game() {
   
   let playerSelection = prompt("Let's play Rock Paper Scissors.");
-
+  
   if (playerScore < 3 && computerScore < 3) {
     if (playRound() == 2) {
       playerScore++;
@@ -27,9 +62,9 @@ function game() {
     return "Congrats! You've won!"
   } else if (playerScore < 3 && computerScore == 3) {
     return "Sorry, you lose. Refresh to play again."
-  }; 
+  }; */
 
-function playRound() {
+function playRound(playerSelection, computerSelection) {
     if (
         playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'paper' ||
@@ -41,7 +76,7 @@ function playRound() {
         playerSelection == 'paper' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'rock'
     ) {
-        return 1;
+         return 1;
     } else if (
         playerSelection == 'rock' && computerSelection == 'rock' ||
         playerSelection == 'scissors' && computerSelection == 'scissors' ||
@@ -49,9 +84,9 @@ function playRound() {
      ) {
         return 0;
     } else {
-        playerSelection = prompt("Please enter either Rock, Paper, or Scissors.")
+        //playerSelection = prompt("Please enter either Rock, Paper, or Scissors.")
     };
   };
-};
+//};
 
-  console.log(game());
+//console.log(gameTest());
